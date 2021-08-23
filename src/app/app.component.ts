e6e615416fb98a780e8c201a1e43719972f69ac0
Luis Gutierrez <luis.gutierrez@se.experis.com>
2021-08-23 14:51:01 +0200

    Crash Course with Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: 'app-root', //html tag to call the component
+  templateUrl: './app.component.html', // html file
+  styleUrls: ['./app.component.css'] // style / css file
 })
-export class AppComponent {
-  title = 'hello-angular';
+export class AppComponent {   //porperties (title), methods, kinda React Class component
+
 }
